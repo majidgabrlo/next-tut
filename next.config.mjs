cd7b2c71159668c638f7bb826497a55a17069fdc
Majid Gabrlo <majidgabrlo@gmail.com>
2024-03-26 02:40:34 +0330

    temp

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,3 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "avatars.githubusercontent.com",
-        },
-      ],
-    },
-  };
-  
-  module.exports = nextConfig;
-  
+const nextConfig = {};
+
+module.exports = nextConfig;
--- src/app/topics/[slug]/page.tsx
+++ src/app/topics/[slug]/page.tsx
@@ -1,4 +1,6 @@
 import PostCreateForm from "@/components/posts/PostCreateForm";
+import PostList from "@/components/posts/PostList";
+import { fetchPostsByTopicSlug } from "@/db/queries/posts";
 
 interface TopicShowPageProps {
   params: { slug: string };
@@ -10,6 +12,7 @@ const TopicShow = ({ params }: TopicShowPageProps) => {
     <div className="grid grid-cols-4 gap-4 p-4">
       <div className="col-span-3">
         <h1 className="text-2xl font-bold mb-2">{slug}</h1>
+        <PostList fetchData={() => fetchPostsByTopicSlug(slug)} />
       </div>
 
       <div>
